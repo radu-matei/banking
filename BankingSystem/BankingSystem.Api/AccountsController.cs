48e741c4da1129f435842dddf2693e293155aca7
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:16:05 +0000

    Added account controller

---
+++ BankingSystem/BankingSystem.Api/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BankingSystem.Api
+{
+    public class AccountsController : Controller
+    {
+        private IAccountManager _accountManager { get; set; }
+
+        public AccountsController(IAccountManager accountManager)
+        {
+            _accountManager = accountManager;
+        }
+
+        public List<Account> GetAccounts()
+        {
+            return _accountManager.GetAccounts();
+        }
+
+        public Account GetAccount(int accountNumber)
+        {
+            return _accountManager.GetAccount(accountNumber);
+        }
+    }
+}
