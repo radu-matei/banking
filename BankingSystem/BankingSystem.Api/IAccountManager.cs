0e78ef8496d10ffb0611cca52eeb36a0b5c345e0
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:14:49 +0000

    Added IAccountManager interface

---
+++ BankingSystem/BankingSystem.Api/IAccountManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BankingSystem.Api
+{
+    public interface IAccountManager
+    {
+        List<Account> GetAccounts();
+        Account GetAccount(int accountNumber);
+        void AddAccount(Account account);
+        bool Contains(Account account);
+    }
+}
