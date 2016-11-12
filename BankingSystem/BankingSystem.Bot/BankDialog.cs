1f40dbbfd8368960ad588a6464081fddb422d714
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:32:25 +0000

    Added dummy BankDialog

---
+++ BankingSystem/BankingSystem.Bot/BankDialog.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BankingSystem.Bot
+{
+    internal class BankDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            await context.PostAsync($"You said: {message.Text}");
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
