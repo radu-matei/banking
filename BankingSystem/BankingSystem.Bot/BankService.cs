30a0fa203c97086cd2d9865a8c45a9474d6deff7
Radu Matei <matei.radu94@gmail.com>
2016-11-12 22:34:48 +0000

    Added basic bank service that communicates with the api

---
+++ BankingSystem/BankingSystem.Bot/BankService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BankingSystem.Bot
+{
+    public class BankService
+    {
+        public async Task<decimal> GetAccountBalance(string accountNumber)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                int number = Convert.ToInt32(accountNumber);
+                client.BaseAddress = new Uri($"http://localhost:5000/api/Accounts/GetAccountBalance?accountNumber={number}");
+
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+                return await response.Content.ReadAsAsync<decimal>();
+            }
+        }
+    }
+}
