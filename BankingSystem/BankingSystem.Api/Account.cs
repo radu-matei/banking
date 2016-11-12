eeae16a0662f41a6b5b8904b4d8c392966f453a1
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:14:41 +0000

    Added Account class

---
+++ BankingSystem/BankingSystem.Api/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace BankingSystem.Api
+{
+    public class Account
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(string owner, int number, decimal balance)
+        {
+            Owner = owner;
+            Number = number;
+            Balance = balance;
+        }
+    }
+}
