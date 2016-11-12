542ee72873d268acb61580cbbf5b1efa6d6c78dc
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:15:33 +0000

    Added TransactionManager implementation

---
+++ BankingSystem/BankingSystem.Api/TransactionManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BankingSystem.Api
+{
+    public class TransactionManager : ITransactionManager
+    {
+        private IAccountManager _accountManager { get; set; }
+
+        public TransactionManager(IAccountManager accountManager)
+        {
+            _accountManager = accountManager;
+        }
+
+        public bool ExcuteTransaction(int sourceAccountNumber, int destinationAccountNumber, decimal amount)
+        {
+            var sourceAccount = _accountManager.GetAccount(sourceAccountNumber);
+            var destinationAccount = _accountManager.GetAccount(destinationAccountNumber);
+
+            if (sourceAccount == null || 
+                destinationAccount == null || 
+                amount < 0)
+                return false;
+
+            sourceAccount.Balance -= amount;
+            destinationAccount.Balance += amount;
+
+            return true;
+        }
+    }
+}
