dfd36c8d5bbe8562f165c649e1b603de0b9d48b9
Radu Matei <matei.radu94@gmail.com>
2016-11-12 20:16:12 +0000

    Added transaction controller

---
+++ BankingSystem/BankingSystem.Api/TransactionsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BankingSystem.Api
+{
+    public class TransactionsController : Controller
+    {
+        private ITransactionManager _transactionManager { get; set; }
+
+        public TransactionsController(ITransactionManager transactionManager)
+        {
+            _transactionManager = transactionManager;
+        }
+
+        [HttpGet]
+        public void ExecuteTransaction([FromQuery]int sourceAccountNumber, [FromQuery]int destinationAccountNumber, [FromQuery]decimal amount)
+        {
+            _transactionManager.ExcuteTransaction(sourceAccountNumber, destinationAccountNumber, amount);
+        }
+    }
+}
